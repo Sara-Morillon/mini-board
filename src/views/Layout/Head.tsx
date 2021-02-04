7c4c2843844c5fd003f07fcd4f0baf17717fb8ef
Your Name <you@example.com>
2021-02-04 20:35:59 +0100

    Remove unused assets

--- src/views/Layout/Head.tsx
+++ src/views/Layout/Head.tsx
@@ -10,13 +10,6 @@ export function Head({ title }: IHeadProps): JSX.Element {
       <title>Scrum board - {title}</title>
       <link rel="icon" type="image/png" href="/favicon.svg" />
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" />
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/diff2html@3.2.0/bundles/css/diff2html.min.css" />
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.5.0/build/styles/default.min.css"
-      />
-      <script type="text/javascript" src="/scripts/copy.js" defer></script>
-      <script type="text/javascript" src="/scripts/change-branch.js" defer></script>
       <script type="text/javascript" src="/scripts/tickets.js" defer></script>
       <style>.d2h-files-diff &#123; height: unset &#125;</style>
     </>
