84738de87c2af1e54d7c972fe5ff2c464340e1b6
Sara Morillon <sara.morillon.pro@gmail.com>
2022-05-23 22:49:59 +0200

    Add logs for login

--- backend/src/controllers/session/postLogin.ts
+++ backend/src/controllers/session/postLogin.ts
@@ -1,15 +1,20 @@
 import { NextFunction, Request, Response } from 'express'
 import passport from 'passport'
+import { start } from '../../libs/logger'
 
 export function postLogin(req: Request, res: Response, next: NextFunction): void {
+  const { success, failure } = start('login', { req: { ...req, body: { ...req.body, password: '*****' } } })
   passport.authenticate('local', function (err, user) {
     if (err || !user) {
+      failure(err)
       res.sendStatus(401)
     } else {
       req.login(user, function (err) {
         if (err) {
+          failure(err)
           res.sendStatus(401)
         } else {
+          success()
           res.sendStatus(204)
         }
       })
