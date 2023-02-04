ec3e2319c1f5e950bbf3c2673fd0fbcdac566dff
Sara Morillon <sara.morillon.pro@gmail.com>
2023-02-04 21:42:50 +0100

    Fix issue form status

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,5 @@
-import 'basscss/css/basscss.min.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import { App } from './ui/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
