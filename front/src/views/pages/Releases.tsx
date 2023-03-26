d9c88129a3c402dca649f694b3711167ee803d03
Sara Morillon <sara.morillon.pro@gmail.com>
2023-03-26 17:08:16 +0200

    Display releases in a table

--- front/src/views/pages/Releases.tsx
+++ front/src/views/pages/Releases.tsx
@@ -1,110 +1,44 @@
-import { useDrag, useDrop, useFetch } from '@saramorillon/hooks'
-import c from 'classnames'
+import { useFetch } from '@saramorillon/hooks'
 import { format, parseISO } from 'date-fns'
-import React, { useCallback, useMemo, useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { useTitle } from '../../hooks/useTitle'
-import { IIssue, IIssueFull } from '../../models/Issue'
-import { IRelease } from '../../models/Release'
-import { getIssues, moveIssue } from '../../services/issue'
 import { getReleases } from '../../services/release'
 import { CreateButton } from '../components/CreateButton'
 import { LoadContainer } from '../components/LoadContainer'
 
 export function Releases(): JSX.Element {
   useTitle('Releases')
-  const [releases, state, refresh] = useFetch(getReleases, [])
-  const [loading, setLoading] = useState(false)
-
-  const onMove = useCallback(
-    (source: IIssue, target: IIssue) => {
-      if (source.id !== target.id) {
-        setLoading(true)
-        moveIssue(source.id, target.id)
-          .then(refresh)
-          .finally(() => setLoading(false))
-      }
-    },
-    [refresh]
-  )
+  const [releases, { loading }] = useFetch(getReleases, [])
 
   return (
     <>
       <div className="clearfix">
         <CreateButton to={`/release`}>Create release</CreateButton>
       </div>
-      <LoadContainer loading={loading || state.loading}>
-        {releases.map((release) => (
-          <Release key={release.id} release={release} onMove={onMove} />
-        ))}
-      </LoadContainer>
-    </>
-  )
-}
 
-interface IReleaseProps {
-  release: IRelease
-  onMove: (source: IIssueFull, target: IIssueFull) => void
-}
-
-function Release({ release, onMove }: IReleaseProps) {
-  const fetch = useCallback(() => getIssues(undefined, release.id), [release.id])
-  const [{ issues }, { loading }] = useFetch(fetch, { issues: [], total: 0 })
-
-  const totalPoints = issues.reduce((acc, curr) => acc + curr.points, 0)
-  const donePoints = issues.filter((issue) => issue.status === 'done').reduce((acc, curr) => acc + curr.points, 0)
-
-  const points = `${issues.length} issue${issues.length > 1 ? 's' : ''} | ${donePoints}/${totalPoints} points`
-  const dueDate = format(parseISO(release.dueDate), 'PP')
-
-  return (
-    <>
-      <h4 className="mt2">
-        <small className="right">{points}</small>
-        <b className="mr1">
-          <Link to={`/release/${release.id}`}>{release.name}</Link>
-        </b>
-        <small>{dueDate}</small>
-      </h4>
       <LoadContainer loading={loading}>
-        {issues.map((issue) => (
-          <IssueCard key={issue.id} issue={issue} onMove={onMove} />
-        ))}
+        <table className="mt2">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Due date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {releases.map((release) => (
+              <tr key={release.id}>
+                <td>
+                  <Link to={`/release/${release.id}`}>{release.name}</Link>
+                </td>
+                <td>
+                  <small>{format(parseISO(release.dueDate), 'PP')}</small>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </LoadContainer>
     </>
-  )
-}
-
-interface IIssueCardProps {
-  issue: IIssueFull
-  onMove: (source: IIssueFull, target: IIssueFull) => void
-}
-
-function IssueCard({ issue, onMove }: IIssueCardProps) {
-  const source = useMemo(() => JSON.stringify(issue), [issue])
-  const onDrop = useCallback((data: string) => onMove(JSON.parse(data), issue), [onMove, issue])
-  const [isDragged, dragEvents] = useDrag(source)
-  const [isOver, dropEvents] = useDrop(onDrop)
-
-  const opacity = isOver ? 0.7 : isDragged ? 0.5 : 1
-
-  return (
-    <article
-      data-testid="issue"
-      {...dragEvents}
-      {...dropEvents}
-      draggable
-      style={{ opacity }}
-      className="flex items-center p1 m0"
-    >
-      <span className={c('mr1', issue.type)} />
-      <div className="truncate flex-auto mr1">
-        <Link to={`/project/${issue.projectId}/issue/${issue.id}`}>
-          [{issue.project.key}-{issue.id}] {issue.title}
-        </Link>
-      </div>
-      <small className="nowrap mr1">{format(parseISO(issue.createdAt), 'PP')}</small>
-      <mark className={issue.status}>{issue.status.toUpperCase()}</mark>
-    </article>
   )
 }
--- front/tests/unit/views/pages/Releases.test.tsx
+++ front/tests/unit/views/pages/Releases.test.tsx
@@ -1,30 +1,20 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import React from 'react'
-import { getIssues, moveIssue } from '../../../../src/services/issue'
 import { getReleases } from '../../../../src/services/release'
 import { Releases } from '../../../../src/views/pages/Releases'
-import { mockIssueFull, mockRelease, wait } from '../../../mocks'
+import { mockRelease, wait } from '../../../mocks'
 
-jest.mock('../../../../src/services/issue')
 jest.mock('../../../../src/services/release')
 
 describe('Releases', () => {
   beforeEach(() => {
     jest.mocked(getReleases).mockResolvedValue([mockRelease()])
-    jest.mocked(getIssues).mockResolvedValue({ issues: [mockIssueFull()], total: 10 })
-    jest.mocked(moveIssue).mockResolvedValue(undefined)
   })
 
   it('should render create button', async () => {
     render(<Releases />)
     await wait()
     expect(screen.getByText('Create release')).toHaveAttribute('href', '/release')
-  })
-
-  it('should render release points', async () => {
-    render(<Releases />)
-    await wait()
-    expect(screen.getByText('1 issue | 0/5 points')).toBeInTheDocument()
   })
 
   it('should render release name', async () => {
@@ -37,57 +27,5 @@ describe('Releases', () => {
     render(<Releases />)
     await wait()
     expect(screen.getByText('Jan 1, 2020')).toBeInTheDocument()
-  })
-
-  it('should render issue title', async () => {
-    render(<Releases />)
-    await wait()
-    expect(screen.getByText('[P1-1] title1')).toHaveAttribute('href', '/project/1/issue/1')
-  })
-
-  it('should render issue created date', async () => {
-    render(<Releases />)
-    await wait()
-    expect(screen.getByText('Jan 1, 2018')).toBeInTheDocument()
-  })
-
-  it('should render issue status', async () => {
-    render(<Releases />)
-    await wait()
-    expect(screen.getByText('DOING')).toHaveClass('doing')
-  })
-
-  it('should set issue opacity to of 0.7 when hovering element', async () => {
-    render(<Releases />)
-    await wait()
-    fireEvent.dragOver(screen.getByTestId('issue'))
-    expect(screen.getByTestId('issue')).toHaveStyle({ opacity: 0.7 })
-  })
-
-  it('should set issue opacity to of 0.5 when element is dragged', async () => {
-    render(<Releases />)
-    await wait()
-    fireEvent.dragStart(screen.getByTestId('issue'), { dataTransfer: { setData: jest.fn() } })
-    expect(screen.getByTestId('issue')).toHaveStyle({ opacity: 0.5 })
-  })
-
-  it('should set issue opacity to of 1 when element is not dragged', async () => {
-    render(<Releases />)
-    await wait()
-    expect(screen.getByTestId('issue')).toHaveStyle({ opacity: 1 })
-  })
-
-  it('should move issue when changing priority', async () => {
-    jest.mocked(getIssues).mockResolvedValue({
-      issues: [mockIssueFull(), mockIssueFull({ id: 2, priority: 2 })],
-      total: 2,
-    })
-    render(<Releases />)
-    await wait()
-    fireEvent.drop(screen.getAllByTestId('issue')[1], {
-      dataTransfer: { getData: jest.fn().mockReturnValue(JSON.stringify(mockIssueFull())) },
-    })
-    await wait()
-    expect(moveIssue).toHaveBeenCalledWith(1, 2)
   })
 })
