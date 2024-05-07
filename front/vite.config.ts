628df8f69102178f83b41cf1d3fa579fe60545c1
Sara Morillon <sara.morillon@gmail.com>
2024-05-07 21:38:40 +0200

    Fix empty ticket height in board

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,18 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { PluginOption, defineConfig } from 'vite'
+
+function publicReload(): PluginOption {
+  return {
+    name: 'public-reload',
+    handleHotUpdate({ file, server }) {
+      if (file.includes('/public/')) {
+        server.ws.send({ type: 'full-reload' })
+      }
+    },
+  }
+}
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), publicReload()],
   server: { port: 4000, proxy: { '/api': 'http://localhost:3000' } },
 })
